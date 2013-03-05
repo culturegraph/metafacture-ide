8248e10f07e35b3f14b8db2f196c045666a11a91
Fabian Steeg <fsteeg@gmail.com>
2013-03-05 17:38:37 +0100

    Adjust build scripts after moving code into new Git repository

--- metatext/deploy.sh
+++ metatext/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 export JAR=target/lodmill-rd-0.1.0-SNAPSHOT-jar-with-dependencies.jar
 export RES=src/main/resources
-cd ../lodmill-rd
+cd ../..
+git clone git://github.com/lobid/lodmill.git lodmill-dependency
+cd lodmill-dependency/lodmill-rd
 mvn clean assembly:assembly -q --settings ../settings.xml
 jar uf $JAR -C $RES metaflow-pipe.properties -C $RES metastream-encoders.properties
-cp $JAR ../metatext/org.lobid.metatext/
+cp $JAR ../../metafacture-ide/metatext/org.lobid.metatext/
