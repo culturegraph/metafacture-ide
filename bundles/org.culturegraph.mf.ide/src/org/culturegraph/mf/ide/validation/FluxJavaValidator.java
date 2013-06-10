85db4eefdf3126e6401f7ad0847eb3f2b00c2de6
Fabian Steeg <fsteeg@gmail.com>
2013-06-10 17:30:11 +0200

    Support `tee` commands (stream-tee, object-tee) in Flux validator

--- bundles/org.culturegraph.mf.ide/src/org/culturegraph/mf/ide/validation/FluxJavaValidator.java
+++ bundles/org.culturegraph.mf.ide/src/org/culturegraph/mf/ide/validation/FluxJavaValidator.java
@@ -4,9 +4,11 @@ package org.culturegraph.mf.ide.validation;
 
 import java.util.Map;
 
+import org.culturegraph.mf.framework.DefaultTee;
 import org.culturegraph.mf.ide.domain.FluxCommandMetadata;
 import org.culturegraph.mf.ide.flux.Flow;
 import org.culturegraph.mf.ide.flux.FluxPackage;
+import org.culturegraph.mf.ide.flux.Mainflow;
 import org.culturegraph.mf.ide.flux.Pipe;
 import org.eclipse.xtext.validation.Check;
 
@@ -21,59 +23,68 @@ import com.google.common.base.Joiner;
  */
 public class FluxJavaValidator extends AbstractFluxJavaValidator {
 
-	private Pipe previousPipe = null;
-
 	private Map<String, FluxCommandMetadata> commands = FluxCommandMetadata
 			.commands();
+
+	private String pipeInput = null;
+	private String teeInput = null;
 
 	@Check
 	private void checkFluxCommand(Pipe pipe) {
 		String commandId = commandId(pipe);
-		if (!commands.containsKey(commandId)) {
+		if (isUnknownCommand(commandId)) {
 			error("Command not found: " + commandId, FluxPackage.Literals.PIPE__QN);
-		}
-		if (firstCommand()) {
-			validateInput(pipe, String.class); /* First command expects String */
+		} else if (isFirstCommand()) {
+			validateInput(commandId, String.class); /* First command expects String */
 		} else {
-			validateInput(pipe, commands.get(commandId(previousPipe)).getOutputType());
+			validateInput(commandId, previousCommand().getOutputType());
 		}
-		previousPipe = pipe;
-	}
-
-	private boolean firstCommand() {
-		return previousPipe == null;
-	}
-
-	private void validateInput(Pipe pipe, Class<?> input) {
-		String commandId = commandId(pipe);
-		FluxCommandMetadata metadata = commands.get(commandId);
-		checkInputAnnotation(commandId, metadata);
-		checkOutputAnnotation(commandId, metadata);
-		boolean isAssignable =
-				/* If no annotations are given, we have to assume it might work: */
-				input == null || metadata.getInputType() == null
-						|| metadata.getInputType().isAssignableFrom(input);
-		if (!isAssignable) {
-			error(String.format(
-					"Command '%s' expects input of type '%s', but got '%s'", commandId,
-					metadata.getInputType().getName(),
-					input == null ? null : input.getName()),
-					FluxPackage.Literals.PIPE__QN);
-		}
+		remember(commandId);
 	}
 
 	private static String commandId(Pipe pipe) {
 		return Joiner.on(".").join(pipe.getQn().getIds());
 	}
 
-	private void checkOutputAnnotation(String commandId,
-			FluxCommandMetadata metadata) {
-		if (metadata.getOutputType() == null) {
-			/* Info only for missing out, since that might not be consumed: */
-			info(String.format(
-					"Implementation '%s' for command '%s' has no @Out annotation",
-					metadata.getImplementationType().getName(), commandId),
-					FluxPackage.Literals.PIPE__QN);
+	private boolean isUnknownCommand(String commandId) {
+		return !commands.containsKey(commandId);
+	}
+
+	private boolean isFirstCommand() {
+		return pipeInput == null && teeInput == null;
+	}
+
+	private FluxCommandMetadata previousCommand() {
+		return commands.get(pipeInput != null ? pipeInput : teeInput);
+	}
+
+	private void remember(String commandId) {
+		if (isTee(commandId)) {
+			/* At a tee, remember the command to continue from in the two branches: */
+			teeInput = pipeInput != null ? pipeInput : teeInput;
+		} else
+			/* But never use the tee command itself as input (hence 'else'): */
+			pipeInput = commandId;
+	}
+
+	private boolean isTee(String commandId) {
+		return DefaultTee.class.isAssignableFrom(commands.get(commandId)
+				.getImplementationType());
+	}
+
+	private void validateInput(String commandId, Class<?> inputType) {
+		FluxCommandMetadata metadata = commands.get(commandId);
+		checkInputAnnotation(commandId, metadata);
+		checkOutputAnnotation(commandId, metadata);
+		boolean isAssignable =
+				/* If no annotations are given, we have to assume it might work: */
+				inputType == null || metadata.getInputType() == null
+						|| metadata.getInputType().isAssignableFrom(inputType);
+		if (!isAssignable) {
+			error(String.format(
+					"Command '%s' expects input of type '%s', but got '%s'", commandId,
+					metadata.getInputType().getName(), inputType == null ? null
+							: inputType.getName()), FluxPackage.Literals.PIPE__QN);
 		}
 	}
 
@@ -89,10 +100,25 @@ public class FluxJavaValidator extends AbstractFluxJavaValidator {
 		}
 	}
 
-	@Check
-	private void resetPipe(@SuppressWarnings("unused") Flow flow) {
-		System.err.println("Resetting flow");
-		previousPipe = null;
+	private void checkOutputAnnotation(String commandId,
+			FluxCommandMetadata metadata) {
+		if (metadata.getOutputType() == null) {
+			/* Info only for missing out, since that might not be consumed: */
+			info(String.format(
+					"Implementation '%s' for command '%s' has no @Out annotation",
+					metadata.getImplementationType().getName(), commandId),
+					FluxPackage.Literals.PIPE__QN);
+		}
 	}
 
+	@Check
+	private void resetAll(@SuppressWarnings("unused") Mainflow flow) {
+		teeInput = null;
+		pipeInput = null;
+	}
+
+	@Check
+	private void resetCurrent(@SuppressWarnings("unused") Flow flow) {
+		pipeInput = null;
+	}
 }
