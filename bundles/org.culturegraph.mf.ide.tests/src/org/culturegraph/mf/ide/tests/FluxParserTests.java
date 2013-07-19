45d0fce2654c3a394d212148a6c652b0e98ce6a3
Fabian Steeg <fsteeg@gmail.com>
2013-07-19 15:28:57 +0200

    Update Flux Xtext grammar for wormhole support (closes #28)
    Also add tests and samples for wormholes and new Flux commands

--- bundles/org.culturegraph.mf.ide.tests/src/org/culturegraph/mf/ide/tests/FluxParserTests.java
+++ bundles/org.culturegraph.mf.ide.tests/src/org/culturegraph/mf/ide/tests/FluxParserTests.java
@@ -26,19 +26,22 @@ public class FluxParserTests extends XtextTest {
 	}
 
 	/* @formatter:off */
-	@Test public void parse01() { testFile("count-gnd-types.flux"); }
-	@Test public void parse02() { testFile("filter-morph.flux"); }
-	@Test public void parse03() { testFile("format-gnd.flux"); }
-	@Test public void parse04() { testFile("MARC21-EDM.flux"); }
-	@Test public void parse05() { testFile("marcxml-online.flux"); }
-	@Test public void parse06() { testFile("morph-marc21.flux"); }
-	@Test public void parse07() { testFile("multiple-flows.flux"); }
-	@Test public void parse08() { testFile("read-dirs.flux"); }
-	@Test public void parse09() { testFile("read-marc21.flux"); }
-	@Test public void parse10() { testFile("read-pica.flux"); }
-	@Test public void parse11() { testFile("regexp.flux"); }
-	@Test public void parse12() { testFile("sort-gnd.flux"); }
-	@Test public void parse13() { testFile("tp2json.flux"); }
+	@Test public void parse01() { testFile("combine-beacon.flux"); }
+	@Test public void parse02() { testFile("count-gnd-types.flux"); }
+	@Test public void parse03() { testFile("filter-morph.flux"); }
+	@Test public void parse04() { testFile("format-gnd.flux"); }
+	@Test public void parse05() { testFile("MARC21-EDM.flux"); }
+	@Test public void parse06() { testFile("marcxml-online.flux"); }
+	@Test public void parse07() { testFile("morph-marc21.flux"); }
+	@Test public void parse08() { testFile("multiple-flows.flux"); }
+	@Test public void parse09() { testFile("read-beacon.flux"); }
+	@Test public void parse10() { testFile("read-dirs.flux"); }
+	@Test public void parse11() { testFile("read-marc21.flux"); }
+	@Test public void parse12() { testFile("read-pica.flux"); }
+	@Test public void parse13() { testFile("regexp.flux"); }
+	@Test public void parse14() { testFile("sort-gnd.flux"); }
+	@Test public void parse15() { testFile("tp2json.flux"); }
+	@Test public void parse16() { testFile("wormhole.flux"); }
 	/* @formatter:on */
 
 	private static ClassLoader contextClassLoader;
