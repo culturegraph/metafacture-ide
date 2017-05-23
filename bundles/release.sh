cdb6597c834e2d09aa1268f8495a2ca1f163d182
Fabian Steeg <fsteeg@gmail.com>
2017-05-23 12:19:08 +0200

    Update version numbers for 0.2.0 release
    See https://github.com/culturegraph/metafacture-ide/issues/42

--- bundles/release.sh
+++ bundles/release.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-scp -r org.culturegraph.mf.ide.p2/target/repository/* lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
-scp org.culturegraph.mf.ide.p2/target/products/*.zip lobid@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools
+scp -r org.culturegraph.mf.ide.p2/target/repository/* steeg@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
+scp -r org.culturegraph.mf.ide.p2/target/org.culturegraph.mf.ide.p2*.zip steeg@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools/p2
+scp org.culturegraph.mf.ide.p2/target/products/*.zip steeg@emphytos.hbz-nrw.de:/usr/local/lobid/src/lobid.org/download/tools
